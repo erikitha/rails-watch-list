ebbbf9110850c5c948d022c225983d865b118108
Erica Park <herykr@hotmail.com>
2022-05-26 16:54:03 -0300

    finished controllers bookmarks and lists

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,6 +1,6 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
-  validates :comment, length: { minimum: 7 }
+  validates :comment, length: { minimum: 6 }
   validates :list_id, uniqueness: { scope: :movie_id }
 end
